1d90c6b38afcbd102150c24979b2f5c3f031b576
rfiggs <rfiggs@hawaii.edu>
2016-10-11 10:10:53 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,30 @@
 console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player' : function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if(playerId ==  selectedPlayer) {
+        return "selected";
+      }
+    },
+  });
+
+  Template.leaderboard.events({
+    // events go here
+    'click .player': function(){
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+
+}
+
